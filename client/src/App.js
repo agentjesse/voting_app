fc434115c4d254e8d7bc3a41a3ca3c41a0a6a169
Jesse <31576321+agentjesse@users.noreply.github.com>
2018-03-15 21:53:39 -0400

    base structure

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,36 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      response: ''
+    }
+  }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err))
+  }
+
+  callApi = async ()=> {
+    const response = await fetch('/api/hello')
+    const body = await response.json()
+    if (response.status !== 200) throw Error(body.message)
+    return body
+  }
+
+
   render() {
     return (
       <div className="App">
-        hello!
+        hello
+        <p>{this.state.response}</p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
