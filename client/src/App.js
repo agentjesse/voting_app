eb19a4fa4d64d73c6e9ed4cacf0828af75cc7379
Jesse <31576321+agentjesse@users.noreply.github.com>
2018-03-17 22:03:22 -0400

    passport functioning middlewares in correct order

--- client/src/App.js
+++ client/src/App.js
@@ -12,23 +12,23 @@ class App extends Component {
   }
 
   componentDidMount() {
-    // this.callApi()
-    //   .then(res => this.setState({ response: res.express }))
-    //   .catch(err => console.log(err))
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err))
   }
 
-  // callApi = async ()=> {
-  //   const response = await fetch('/api/hello')
-  //   const body = await response.json()
-  //   if (response.status !== 200) throw Error(body.message)
-  //   return body
-  // }
+  callApi = async ()=> {
+    const response = await fetch('/api/hello')
+    const body = await response.json()
+    if (response.status !== 200) throw Error(body.message)
+    return body
+  }
 
 
   render() {
     return (
       <div className="App">
-        {/* <p>{this.state.response}</p> */}
+        <p>{this.state.response}</p>
         <Chart />
       </div>
     )
--- server.js
+++ server.js
@@ -1,18 +1,92 @@
 //packages
-const express = require('express')
 require('dotenv').config()
+const express = require('express')
+, session = require('express-session')
+, passport = require('passport')
+, TwitterStrategy = require('passport-twitter').Strategy
+
+//setup passport
+passport.use(new TwitterStrategy({
+  consumerKey: process.env.TWITTER_CONSUMER_KEY,
+  consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+  callbackURL: "http://localhost:5000/auth/twitter/callback"
+},
+(token, tokenSecret, profile, done) => {
+    //purpose of this verify callback: find user matching credential arguments. When Passport authenticates a request, it parses the credentials contained in the request. It then invokes this verify callback with the credentials. You then check if they're valid and invokes done() to supply Passport with the user that authenticated.
+
+    // User.findOrCreate(..., function(err, user) {
+    //   if (err) { return done(err); }
+    //   done(null, user);
+    // });
+    // console.log('twitter username: ', profile.username)
+    done(null, profile.username) //supply Passport with the user that authenticated
+    // done(null,false) //failed authentication on your end. (remember twitter will only return with credentials of valid twitter accounts)
+  }
+) )
+passport.serializeUser( (user, done) => {
+  done(null, user)
+})
+passport.deserializeUser( (user, done) => {
+  done(null, user)
+})
 
 //create app
 const app = express()
 const port = process.env.PORT || 5000
 
 //middleware
-app.use((req,res)=>{ console.log(`request made of type: ${req.method}`) })
+  // configure/use session middleware BEFORE PASSPORT!!!!
+  app.use(session( {secret:'whatever', resave:true, saveUninitialized:true} ) )
+  
+  //use passport
+  app.use(passport.initialize())
+  app.use(passport.session())
+  
 
-//routes
-app.get('/api/hello', (req, res) => {
-  res.send({ express: '...Hello From Express' })
-})
+  //parse requests with urlencoded payloads to make req.body object
+  app.use('/', express.urlencoded({extended:false}) )
+
+  //log all requests received
+  // app.use( /^\S+/, (req,res,next) => { //this route path matches 1 or more non whitespace chars
+  app.use( '/', (req,res,next) => { //matches /, /apples, /apples/bears
+    console.log(`${req.method} request received with url: ${req.originalUrl}`)
+    next()
+  })
+
+  // Redirect the user to Twitter for authentication.  When complete, Twitter will redirect the user back to /auth/twitter/callback
+  app.get('/auth/twitter', passport.authenticate('twitter') )
+
+  // Twitter will redirect the user to this URL after approval.  Finish the authentication process by attempting to obtain an access token. If access was granted, the user will be logged in.  Otherwise, authentication has failed.
+  app.get('/auth/twitter/callback',
+    passport.authenticate('twitter', { successRedirect: '/poo', failureRedirect:'/poo2' } )
+  )
+
+  //testing file return and form submission
+  /**/
+  app.get( '/poo', (req,res,next) => {
+    console.log(req.user)
+    res.sendFile( `${__dirname}/test.html` )
+    // next()
+  })
+  app.get( '/poo2', (req,res,next) => { //this will never get called since verify callback only implements done(null, user)
+    // console.log(`${ req.user ? 'authorized user' : 'Unauthorized user' }`)
+    console.log(`${ req.user }`)
+    res.sendFile( `${__dirname}/test2.html` )
+    // next()
+  })
+  app.post( '/give_food', (req,res,next) => {
+    console.log(req.headers['content-type'], req.body)
+    res.end()
+  })
+  /**/
+
+
+  //uncomment when production build is ready in client/build folder
+  // app.use(‘/’, express.static(`${__dirname}/client/build`))
+
+  app.get('/api/hello', (req,res) => {
+    res.send({ express: '...Hello From Express' })
+  })
 
 //error handler
 app.use((err,req,res,next)=> {
