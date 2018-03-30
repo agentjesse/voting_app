c001129abd46d19aa58a6a3d3d08514bd0611d29
Jesse <31576321+agentjesse@users.noreply.github.com>
2018-03-29 22:29:48 -0400

    properly set up chart.js module with ref system

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import Chart from './Chart'
 import './App.css'
 
 class App extends Component {
-
   constructor(props) {
     super(props)
     this.state = {
@@ -17,7 +16,7 @@ class App extends Component {
       .catch(err => console.log(err))
   }
 
-  callApi = async ()=> {
+  callApi = async () => {
     // console.log(`making fetch`)
     const response = await fetch('/api/hello') //this will pass through the proxy for sure. not sure about browser based url requests...maybe react-router will handle because port 3000 is forced there?
     const body = await response.json()
@@ -26,18 +25,18 @@ class App extends Component {
     return body
   }
 
-  getGarbage = async ()=> {
+  getGarbage = async () => {
     try {
       let response = await fetch('/garbage')
       if (response.ok) {
         let jsonResponse = await response.json()
-        console.log( `server reply:`, jsonResponse )
+        console.log(`server reply:`, jsonResponse)
         // console.log( `server reply:`, response )
         return //return now, or error below will fire after this conditional
       }
       throw new Error('Request failed!')
     }
-    catch (err) { console.log('tried and caught error:',err) }
+    catch (err) { console.log('tried and caught error:', err) }
   }
 
   render() {
--- client/src/Chart.js
+++ client/src/Chart.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
+import Chart from 'chart.js'
 const randomColor = require('randomcolor')
 
-//note this component cant be named Chart because it would interfere with the global of same name.
 class MyChart extends Component {
   constructor(props) {
     super(props)
@@ -11,17 +11,17 @@ class MyChart extends Component {
   //never rerender this component
   shouldComponentUpdate() { return false }
 
-  //the refs system will call this function when the element has rendered. and this will occur before lifecycle method componentDidMount/componentDidUpdate
-  makeChart(canvas){
-    //eslint-disable-next-line
-    const myDoughnutChart = new Chart( canvas, { //Chart here comes from the global import
+  //the refs system will call this function when the element has rendered. and this will occur before lifecycle methods: componentDidMount/componentDidUpdate
+  makeChart(canvas) {
+    // const myDoughnutChart = new Chart( canvas, { //no need to save to variable
+    new Chart(canvas, {
       type: 'doughnut',
       data: {
         // These labels appear in the legend and in the tooltips when hovering different arcs
         labels: ['option1','option2','option3','option4','option5','option6'],
         datasets: [
           {
-            data: [10, 10, 10,10,20,40],
+            data: [10, 10, 10, 10, 20, 40],
             backgroundColor: randomColor( {luminosity: 'light', count:6} ) //returns array of 6 colors
           }
         ]
@@ -30,7 +30,7 @@ class MyChart extends Component {
     })
   }
 
-  //ref system used to save a direct refrence to a dom element
+  //ref system used to save a direct refrence to a dom element to be used with the chart js library that manipulates the canvas element from the dom
   render() {
     return (
       <div style={ {position: 'relative', width:'100%' } }>
